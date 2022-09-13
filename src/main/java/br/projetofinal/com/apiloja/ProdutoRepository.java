830cfd5e4d864f76eaeba871d453497187c30587
Germanir <67175588+Germanir@users.noreply.github.com>
2022-09-13 12:50:53 -0300

    final api

--- src/main/java/br/projetofinal/com/apiloja/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.projetofinal.com.apiloja;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
-    
-}
